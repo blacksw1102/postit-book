1d53bdb3735e1f4a95ad1f5012cac57dcc37c0ce
suwan <blacksw1102@gmail.com>
2022-05-15 23:50:00 +0900

    bootstrap 추가

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/html/index.html");
 });
 
+app.get("/list", function (req, res) {
+  res.sendFile(__dirname + "/html/list.html");
+});
+
 app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/html/about.html");
 });
 
-app.listen(port, () => { 
+app.listen(port, () => {
   console.log(`Server started. port ${port}.`);
 });
